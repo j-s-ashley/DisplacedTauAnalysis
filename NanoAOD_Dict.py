f323e8fdc3aa823cc23ded86a1fb1e93a9c6444b
j-s-ashley <jashley6@vols.utk.edu>
2024-10-20 09:48:08 -0400

    Add full path to BG and signal files

--- NanoAOD_Dict.py
+++ NanoAOD_Dict.py
@@ -1,9 +1,9 @@
 Nano_Dict = {
-  "Run3_TT_13p6TeV" : "080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root",
+  "Run3_TT_13p6TeV" : "/eos/user/d/dally/DisplacedTauAnalysis/080924_BG_Out/TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/crab_TTtoLNu2Q_TuneCP5_13p6TeV_powheg-pythia8/240809_215454/0000/nanoaod_output_1.root",
   #"Run3_Stau_500GeV_100mm":"Stau_M_500_100mm_Summer22EE_NanoAOD.root", 
   #"Run3_Stau_500GeV_10mm": "Stau_M_500_10mm_Summer22EE_NanoAOD.root", 
   #"Run3_Stau_100GeV_10mm": "Stau_M_100_10mm_Summer22EE_NanoAOD.root", 
-  "Run3_Stau_100GeV_100mm" : "SMS-TStauStau_MStau-100_ctau-100mm_mLSP-1_TuneCP5_13p6TeV_NanoAOD.root",
+  "Run3_Stau_100GeV_100mm" : "/eos/user/d/dally/DisplacedTauAnalysis/SMS-TStauStau_MStau-100_ctau-100mm_mLSP-1_TuneCP5_13p6TeV_NanoAOD.root",
   #"Run2_Stau_400GeV_1000mm": "Stau_M_400_1000mm_Summer18UL_NanoAOD.root",
   #"Run2_Stau_400GeV_100mm": "Stau_M_400_100mm_Summer18UL_NanoAOD.root",
   #"Run2_Stau_100GeV_100mm" : "Stau_M_100_100mm_Summer18UL_NanoAOD.root",
