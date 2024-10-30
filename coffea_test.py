a285e08869e3e60fda6463987b5ed4d49b0f0fc3
j-s-ashley <jashley6@vols.utk.edu>
2024-10-30 15:33:21 -0400

    Remove debug prints and comments

--- coffea_test.py
+++ coffea_test.py
@@ -15,7 +15,6 @@ NanoAODSchema.warn_missing_crossrefs = False
 
 file = "/eos/user/d/dally/DisplacedTauAnalysis/SMS-TStauStau_MStau-100_ctau-100mm_mLSP-1_TuneCP5_13p6TeV_NanoAOD.root"
 
-#events = NanoEventsFactory.from_root({file:"Events"}, schemaclass=NanoAODSchema).events()[0:21]
 events = NanoEventsFactory.from_root({file:"Events"}).events()
 
 gpart = events.GenPart
@@ -23,7 +22,6 @@ electrons = events.Electron
 muons = events.Muon
 staus = gpart[(abs(gpart.pdgId) == 1000015) & (gpart.hasFlags("isLastCopy"))]
 
-#taus = gpart[(abs(gpart.pdgId) == 15) & (abs(gpart.parent.pdgId) == 1000015) & (gpart.hasFlags("isLastCopy"))]
 staus_taus = staus.distinctChildren[(abs(staus.distinctChildren.pdgId) == 15) & (staus.distinctChildren.hasFlags("isLastCopy"))]
 staus_taus = ak.flatten(staus_taus, axis=2)
 
@@ -34,14 +32,4 @@ RecoElectronsFromGen = electrons[(abs(electrons.matched_gen.distinctParent.disti
 gen_electrons = gen_electrons[(gen_electrons.pt > 20) & (abs(gen_electrons.eta) < 2.4)]
 RecoElectronsFromGen = RecoElectronsFromGen.matched_gen[(RecoElectronsFromGen.matched_gen.pt > 20) & (abs(RecoElectronsFromGen.matched_gen.eta) < 2.4)]
 
-
 makeEffPlot("e", "coffea_100GeV_100mm", [""], "pt", 16, 20, 100, 5, "[GeV]", [gen_electrons.pt.compute()], [RecoElectronsFromGen.pt.compute()], 0, file) 
-
-print("Event fields are", events.event.fields)
-
-for ev in range(ak.num(events, axis = 0).compute()):
-  print("The pdgId of the mus in event", ev, "is", gen_mus.pt.compute()[ev])
-  print("The pt of the electrons in event", ev, "is", gen_electrons.pt.compute()[ev])
-  print("The gen idx of the matched muon for event", ev, "is", electrons.matched_gen.compute()[ev]) 
-  print("The dr between reco ele and gen ele for event", ev, "is", dr.compute()[ev])
-  print("The reco electrons that trace back to gen electrons whcih decay from staus for event", ev, "are", RecoElectronsFromGen.genPartIdx.compute()[ev]) 
